e946ba3bf6b5955be2a3d78fbacec3c7cf1c993d
Khizer Younas <m.khizeryounas@gmail.com>
2019-12-25 02:36:01 +0500

    feat: initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Demo files for code style guide
+ * @module Main
+ */
+
+/**
+ * Add two numbers
+ * @param {Number} a first number
+ * @param {Number} b second number
+ * @returns {Number} result of the function
+ * @example
+ * let x = addNumbers(2, 2);
+ * @see https://github.com/mkhizeryounas
+ * @todo Stuff left todo
+ */
+const addNumbers = (a, b) => {
+  return a + b;
+};
+
+module.exports = { addNumbers };
